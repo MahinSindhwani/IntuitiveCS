6e81555e91032011b0baf6a7a727f58aa79350d0
Mahin Sindhwani <mahinsindhwani@gmail.com>
2024-12-31 10:43:09 -0330

    -

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import HamburgerMenu from "./HamburgerMenu.jsx";
 
 function Main() {
   return (
-    <Router basename="/IntuitiveCS">
+    <Router basename="/IntuitiveCS/">
       <HamburgerMenu />
       <Routes>
         <Route path="/" element={<Home />} />
